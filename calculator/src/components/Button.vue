e9340c2c388e662202c1f6fb27d8a35bd0b3f742
Seph <paulo.seph@hotmail.com>
2020-01-02 10:39:04 -0300

    Atualizando projeto

--- calculator/src/components/Button.vue
+++ calculator/src/components/Button.vue
@@ -1,15 +1,76 @@
 <template>
-  <button class="button">
-      0
+<!-- Aqui em :class se double for true ele chama o double e aplica a classe
+para esse botão, la em baixo foi definido o CSS com cada classe -->
+  <button class="button"
+  :class="{ double, tripe, operation }">
+      {{ label }}
+      {{seinao}}
+      
   </button>
 </template>
 
 <script>
 export default {
+    // o componente não tem que ser exportado? O>O
+    // Acho que é por causa da pasta components
+    data() {
+        return {
+            lala: "Stringzona"
+        }
+    },
+    
 
+    // Aqui em props está definindo propriedades para o componente, no caso Button.
+    // As propriedades só podem ser utilizadas dentro dele.
+    // Não são variaveis.
+    props: {
+        label: {},
+        operation: { type: Boolean},
+        double: { type: Boolean},
+        tripe: { type: Boolean},
+        seinao: {
+                   }          
+        
+    }
 }
 </script>
 
 <style>
+/* Definindo variavel */
+:root {
+    --bg-button: #f0f0f0;
+    --border-button: solid 1px #888;
+}
+
+.button {
+    font-size: 1.4em;
+    background-color: var(--bg--button);
+    border: none;
+    border-right: var(--border-button);
+    border-bottom: var(--border-button);
+    outline: none;
+}
+/* Definindo css para os botoes, no caso as classes
+as que seram classe como double, tripe, e operation */
+.button:active {
+    background-color: #ccc
+}
+
+.button.double {
+    grid-column: span 2;
+}
+
+.button.tripe{
+    grid-column: span 3;
+}
+
+.button.operation {
+    background-color: #fa8231;
+    color: #fff;
+}
+
+.button.operation:active {
+    background-color: #fa8231cc
+}
 
 </style>
--- calculator/src/main/Calculator.vue
+++ calculator/src/main/Calculator.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="calculator">
       <Display/>
-      <Button/>
+      <Button label="AC" triple/>
+      <Button label="/"/>
+      <Button label="7"/>
+      <Button label=""/>
+      <Button label=""/>
+      <Button label=""/>
+      <Button label=""/>
+      <Button label=""/> 
+      <Button label=""/>
+      <Button label=""/>
+      <Button label=""/>
+      <Button label=""/>
+      <Button label=""/>
+      <Button label=""/>
+      <!-- Aqui nao consegue acessar a variavel definida em data do arquivo do componente Button.vue -->
+      {{lala}}
+    <!-- A propriedade(props) é dentro do Button -->
+
+    <Button seinao="aa"></Button>
+    <!-- Dessa forma não é possivel.
+    Tem que usar o  </slot> para associar-->
+    <Button>{{seinao}}</Button>
   </div>
 </template>
 
@@ -17,5 +38,13 @@ export default {
 <style>
     .calculator {
         height: 320px;
+        width: 235px;
+        border-radius: 5px;
+        overflow: hidden;
+
+        display: grid;
+        grid-template-columns: repeat(4, 25%);
+        grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+        
     }
 </style>
